d60bf9bf50ce5f921ec841c0d89cf81fb82fd802
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-01-17 16:48:58 +0000

    Page now asks for players names and displays them

--- app.rb
+++ app.rb
@@ -1,17 +1,12 @@
 require 'sinatra/base'
+require "sinatra"
 
-class Battle < Sinatra::Base
-  get '/battle' do
-    erb(:index)
-  end
+get '/' do
+  erb(:index)
+end
 
-  post '/names' do
-    p params
-    @name1 = params[:name1]
-    @name2 = params[:name2]
-    erb(:play)
-  end
-
-  # start the server if ruby file executed directly
-  run! if app_file == $0
+post '/names' do
+  @player1_name = params[:player1_name]
+  @player2_name = params[:player2_name]
+  erb(:play)
 end
