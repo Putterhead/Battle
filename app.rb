e0bfa42166d4a54d6d50d0f25ee6336362c9327e
PeteGH <peter@grant-hay.eu>
2017-01-18 16:13:32 +0100

    adding logic to the model ch.23

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require "sinatra"
+require "./lib/player"
 
 class Battle < Sinatra::Base
 enable :sessions
@@ -10,20 +11,20 @@ set :session_secret, "super secret"
   end
 
   post '/names' do
-    session[:player1_name] = params[:player1_name]
-    session[:player2_name] = params[:player2_name]
+    $player1 = Player.new(params[:player1_name])
+    $player2 = Player.new(params[:player2_name])
     redirect('/play')
   end
 
   get '/play' do
-    @player1_name = session[:player1_name]
-    @player2_name = session[:player2_name]
+    @player1_name = $player1.player_name
+    @player2_name = $player2.player_name
     erb(:play)
   end
 
   get '/attack' do
-    @player1_name = session[:player1_name]
-    @player2_name = session[:player2_name]
+    @player1_name = $player1.player_name
+    @player2_name = $player2.player_name
     erb(:attack)
   end
 
