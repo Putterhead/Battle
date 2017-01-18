d4cecc5efe996f216b51198a2a8a339bb0c55b33
PeteGH <peter@grant-hay.eu>
2017-01-18 13:39:35 +0100

    adding ch.19 Post-redirect loop

--- app.rb
+++ app.rb
@@ -10,8 +10,14 @@ set :session_secret, "super secret"
   end
 
   post '/names' do
-    @player1_name = params[:player1_name]
-    @player2_name = params[:player2_name]
+    session[:player1_name] = params[:player1_name]
+    session[:player2_name] = params[:player2_name]
+    redirect('/play')
+  end
+
+  get '/play' do
+    @player1_name = session[:player1_name]
+    @player2_name = session[:player2_name]
     erb(:play)
   end
   run! if app_file == $0
